fc7c2d77ecd167a091f927a70619a7f73cd23ba3
shrikrushana <papupingale123@gmail.com>
2023-05-31 22:31:48 +0530

    test

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0xBc849f747Fd44ef3f8a4b08203e08E618AD1b835";
+export const contractABI = abi.abi;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+//souWHsl0W3ZALCHcLkhMziNWHD7PAEYs
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/MF8ZunT8hxQPHi9s2_rGjixjaIdVF582',
+      accounts: ['b449b9ef89b28ad80a90f4dd51cadb72535f0a4435694b4b304866cd0679c798'],
+    },
+  },
+};
